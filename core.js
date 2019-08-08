207fd7f34ecda7022cda921556008bb01e6bf068
Purushottam Shukla <formalspace@gmail.com>
2019-08-09 02:32:13 +0530

    postback_data added || Single lib for sending & receving

--- core.js
+++ core.js
@@ -1,45 +1,70 @@
 console.log("Corejs loaded")
-class CrossData {
-    constructor(requestor){
+class Bifrost {
+    constructor(address){
         this.incoming        
-        this.requestor =  requestor
+        this.address =  address
+        this.midgard = document.getElementById("cross-data")
+        window.addEventListener("message", (e) => {
+            if(e.origin === this.address){
+                if(e.data.type === "bifrost-request-data"){
+                    this.rainbowbridge("postback_data")
+                }
+            }
+        })
     }
     async getData(key){
         this.rainbowbridge("get_data")
-        var crossData  = document.getElementById("cross-data")
-        let message = {
-            key :key,
-            type: "crossdata-request-data"
-        }
-        crossData.contentWindow.postMessage(message, '*')
+        this.rainbowbridge("request_data",key)
         return await this.incoming
-
     }
 
-    rainbowbridge(event){
-        console.log("Buring Rainbow Bridge ")
+    rainbowbridge(event,payload){ // Burning Rainbow Bridge of Bifrost
+        console.log("Burning Rainbow Bridge ")
         switch(event){
             case "get_data":
-            this.incoming = new Promise((resolve, reject) => {
+                this.incoming = new Promise((resolve, reject) => {
                                     window.addEventListener("message", (e) => {
-                                        if(e.origin === this.requestor){
-                                            if(e.data.type){
-                                                if(e.data.type === "crossdata-requested-data"){
-                                                    console.log("This sis ",e.data.value)
-                                                    resolve(e.data.value)
-                                                }
+                                        if(e.origin === this.address){
+                                            if(e.data.type && e.data.type === "bifrost-requested-data"){
+                                                console.log("This sis ",e.data.value)
+                                                resolve(e.data.value)
                                             }
-                                        }
-                                    
-                                })    
-                            })
-            break;
+                                        }    
+                                    })    
+                                })
+                break;
+
+            case "request_data":
+                let message = {
+                    key :payload,
+                    type: "bifrost-request-data"
+                }
+                this.midgard.contentWindow.postMessage(message, '*')
+                break;
+
+            
+            case "postback_data":
+                if(typeof(payload) === "object"){
+                    let value = []
+                    payload.map(key => value.push(localStorage.getItem(key)))
+                    let message = {
+                        type: "crossdata-requested-data",
+                        value : value 
+                    }
+                    window.parent.postMessage(message,"*")      
+                }else{
+                    let message = {
+                        type : "crossdata-requested-data",
+                        value : localStorage.getItem(key)
+                    }
+                    window.parent.postMessage(message,"*")
+                }
         }
 
        
     }
 }
 
-export default CrossData
+export default Bifrost
 
 
