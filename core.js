67e5da265c9945be2668c594e29ac1a5c289de49
Purushottam Shukla <formalspace@gmail.com>
2019-08-09 03:57:50 +0530

    Set Data Functionality added

--- core.js
+++ core.js
@@ -1,7 +1,7 @@
 console.log("Corejs loaded")
 class Bifrost {
     constructor(address){
-        this.incoming        
+        this.bifrostResponse        
         this.address =  address
         this.midgard = document.getElementById("cross-data")
         window.addEventListener("message", (e) => {
@@ -9,56 +9,92 @@ class Bifrost {
                 if(e.data.type === "bifrost-request-data"){
                     this.rainbowbridge("postback_data",e.data.value)
                 }
+                if(e.data.type === "bifrost-set-data"){
+                    this.rainbowbridge("postback_set_data",e.data.value)
+                }
             }
         })
     }
     async getData(key){
-        this.rainbowbridge("get_data")
+        this.rainbowbridge("get_response")
         this.rainbowbridge("request_data",key)
-        return await this.incoming
+        return await this.bifrostResponse
+    }
+
+    async setData(payload){
+        this.rainbowbridge("set_data",payload)
+        this.rainbowbridge("get_response")
     }
 
     rainbowbridge(event,payload){ // Burning Rainbow Bridge of Bifrost
         console.log("Burning Rainbow Bridge ",event,payload)
         switch(event){
-            case "get_data":
-                this.incoming = new Promise((resolve, reject) => {
+            case "get_response":
+                this.bifrostResponse = new Promise((resolve, reject) => {
                                     window.addEventListener("message", (e) => {
                                         if(e.origin === this.address){
-                                            if(e.data.type && e.data.type === "bifrost-requested-data"){
-                                                console.log("This sis ",e.data.value)
+                                            if(e.data.type && e.data.type === "bifrost-response"){
+                                                console.log("This Response ",e.data.value)
                                                 resolve(e.data.value)
                                             }
                                         }    
                                     })    
                                 })
-                break;
+            break;
 
             case "request_data":
-                let message = {
+                let request_data = {
                     type: "bifrost-request-data",
                     value :payload
                 }
-                this.midgard.contentWindow.postMessage(message, '*')
-                break;
+                this.midgard.contentWindow.postMessage(request_data, '*')
+            break;
 
-            
+
+            case "set_data":
+                let set_data = {
+                    type: "bifrost-set-data",
+                    value: payload
+                }
+                this.midgard.contentWindow.postMessage(set_data, '*')
+            break;
+
+                
             case "postback_data":
                 if(typeof(payload) === "object"){
                     let value = []
                     payload.map(key => value.push(localStorage.getItem(key)))
                     let message = {
-                        type: "bifrost-requested-data",
+                        type: "bifrost-response",
                         value : value 
                     }
                     window.parent.postMessage(message,"*")      
                 }else{
                     let message = {
-                        type : "bifrost-requested-data",
+                        type : "bifrost-response",
                         value : localStorage.getItem(payload)
                     }
                     window.parent.postMessage(message,"*")
                 }
+            break;
+
+            case "postback_set_data":
+                    localStorage.setItem(payload["key"],payload["value"])
+                    if(localStorage.getItem(payload["key"])){
+                        let message = {
+                            type: "bifrost-response",
+                            value : true 
+                        }
+                        window.parent.postMessage(message,"*") 
+                    }else{
+                        let message = {
+                            type: "bifrost-response",
+                            value : false 
+                        }
+                        window.parent.postMessage(message,"*") 
+                    }
+            break;
+
         }       
     }
 }
